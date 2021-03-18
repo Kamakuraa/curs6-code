39299f36c4635f78b27eeeb1076dfff4a398313f
Alex Bratu <a.cosmin.bratu@gmail.com>
2021-03-18 20:09:31 +0200

    Initial commit

---
+++ src/ro/fastrackit/curs6/Main.java
@@ -0,0 +1,7 @@
+package ro.fastrackit.curs6;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/ro/fastrackit/curs6/Redact.java
@@ -0,0 +1,7 @@
+package ro.fastrackit.curs6;
+
+public class Redact {
+    public static void main(String[] args) {
+        
+    }
+}
